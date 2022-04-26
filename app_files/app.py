a8fcb11a34ef270d5e245cd223cd10fd13001320
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-04-26 17:11:02 +0300

    app: calculator

--- app_files/app.py
+++ app_files/app.py
@@ -17,3 +17,34 @@ def greet(username):
     return render_template(
         'greet_templ.html',
         username = escape(username))
+
+@app.route('/calc/')
+def calc():
+    return render_template('calc.html')
+
+@app.route('/calc/<operator>/<int:num1>/<int:num2>/')
+def calc_result(operator, num1, num2):
+    operator = escape(operator) 
+    if operator == "add":
+        procedure = f'{num1} + {num2}'
+        result = num1 + num2
+    
+    if operator == "subtract":
+        procedure = f'{num1} - {num2}'
+        result = num1 - num2
+    
+    if operator == "multiply":
+        procedure = f'{num1} * {num2}'
+        result = num1 * num2
+
+    if operator == "divide":
+        try:
+            result = num1 / num2
+            procedure = f'{num1} * {num2}'
+        except ZeroDivisionError:
+            return "Can't divide by zero"
+
+    return render_template(
+        'calc_result_templ.html',
+        procedure = procedure,
+        result = result)
