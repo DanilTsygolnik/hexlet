6f6b8ade9385310d13dba4252bc88578f5abba6e
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-04-26 15:15:07 +0300

    app: hello -> index

--- app_files/app.py
+++ app_files/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return render_template('hello.html')
+def index():
+    return render_template('index.html')
